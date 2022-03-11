4592ba0401b81dbd28ca70ab7642826de1757c91
Wenzel Massag <wmassag@wizard-of-ulron.local>
2022-03-11 13:01:37 +0000

    First simple graphic

--- Unit-2/VariablesAndFunctions.js
+++ Unit-2/VariablesAndFunctions.js
@@ -1,3 +1,24 @@
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+
+context.fillStyle = 'red';
+context.fillRect(100,100,400,400);
+
+context.lineWidth = 4;
+context.beginPath();
+context.rect(90, 90, 420, 420);
+context.stroke();
+
+context.beginPath();
+context.arc(300, 300, 123, Math.PI * 2, Math.PI);
+context.stroke();
+
+context.strokeStyle = "white"
+context.beginPath();
+context.arc(300, 300, 123, Math.PI, Math.PI * 2);
+context.stroke();
+
+
 let year = 2042;
 let nextYear = year++;
 
